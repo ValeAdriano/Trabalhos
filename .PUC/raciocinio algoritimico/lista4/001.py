93be76183eee539f09e314b40c99348fe90b4a17
AdrianoVale1603 <78498215+AdrianoVale1603@users.noreply.github.com>
2023-05-08 14:28:57 -0300

    .

--- .PUC/raciocinio algoritimico/lista4/001.py
+++ .PUC/raciocinio algoritimico/lista4/001.py
@@ -1,40 +1,33 @@
-estoque = {'pao': 10, 'hamburguer': 12, 'tomate': 5, 'bacon': 5, 'ovo': 5}
-cardapio = {
-    'x-burguer': ['pao', 'hamburguer'],
-    'x-salada': ['pao', 'hamburguer', 'tomate'],
-    'x-bacon': ['pao', 'hamburguer', 'tomate', 'bacon'],
-    'x-egg': ['pao', 'hamburguer', 'ovo'],
-    'x-tudo': ['pao', 'hamburguer', 'tomate', 'hamburguer', 'bacon', 'ovo']
+print('-=-'*10)
+print('NAVEGADOR INTERESTELAR')
+print('-=-'*10)
+
+anos_luz = {
+ "pc": 0.31,
+ "al": 1,
+ "ae": 63241.09,
+ "ml": 525960.23,
+ "sl": 31557609.92
 }
-pedidos = []
 
-while True:
-    print("=== Cardápio ===")
-    for produto, ingredientes in cardapio.items():
-        print(f"{produto}: {', '.join(ingredientes)}")
-    pedido = input("O que deseja pedir (0 para sair)? ")
-    if pedido == '0':
-        break
-    elif pedido not in cardapio:
-        print("Item não localizado no cardápio")
-    else:
-        ingredientes_faltantes = []
-        for ingrediente in cardapio[pedido]:
-            if estoque.get(ingrediente, 0) == 0:
-                ingredientes_faltantes.append(ingrediente)
-        if ingredientes_faltantes:
-            for ingrediente in ingredientes_faltantes:
-                print(f"Infelizmente acabou o {ingrediente}")
-        else:
-            for ingrediente in cardapio[pedido]:
-                estoque[ingrediente] -= 1
-            pedidos.append(pedido)
-            print(f"Um {pedido} saindo no capricho!!!")
+unidades = ["Parsec (pc)", "Ano-Luz (al)", "Unidade Astronômica (ae)", "Minuto-Luz (ml)", "Segundo-Luz (sl)"]
 
-print("=== Pedido Finalizado ===")
-print("Lanches pedidos:")
-for pedido in pedidos:
-    print(f"- {pedido}")
-print("Ingredientes restantes em estoque:")
-for ingrediente, quantidade in estoque.items():
-    print(f"{ingrediente}: {quantidade}")
+# Imprime a lista de unidades de conversão
+print("Unidades de tempo disponíveis para conversão:")
+for unidade in unidades:
+    print("- " + unidade)
+
+# Solicita o valor que se deseja converter
+valor = float(input("Valor a ser convertido: "))
+
+# Solicita a unidade origem do valor
+unidade_origem = input("Converter de: ")
+
+# Solicita a unidade destino de conversão
+unidade_destino = input("Converter para: ")
+
+# Calcula a conversão de unidades
+valor_convertido = valor * anos_luz[unidade_origem] / anos_luz[unidade_destino]
+
+# Exibe o valor convertido
+print(f"Conversão: {valor} {unidade_origem} = {valor_convertido} {unidade_destino}")
