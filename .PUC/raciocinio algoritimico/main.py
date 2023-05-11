6ab9d7571ff5b45db34622ede476307690992c72
Adriano Vale <acsvalefilho@gmail.com>
2023-05-11 12:31:12 -0300

    GP

---
+++ .PUC/raciocinio algoritimico/main.py
@@ -0,0 +1,17 @@
+'''
+Sistema de controle de vendas - Bebidas
+'''
+
+def menu():
+    msg = '** Sstema de Vendas **'
+    print(len(msg) * '*')
+    print(msg)
+    print('1. mod de clientes')
+    print('2. mod de produtos')
+    print('3. mod de vendas')
+    print('4. sair')
+    print(len(msg) * '*')
+    op = input('')
+
+
+menu()
