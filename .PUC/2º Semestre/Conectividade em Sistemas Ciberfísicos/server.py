a3f64c8d3f1fe6fb38649a9e73b552b53c764bd3
Adriano Vale <acsvalefilho@gmail.com>
2023-11-08 12:21:05 -0300

    .

--- .PUC/2º Semestre/Conectividade em Sistemas Ciberfísicos/server.py
+++ .PUC/2º Semestre/Conectividade em Sistemas Ciberfísicos/server.py
@@ -1,4 +1,20 @@
 import socket
+import threading
+
+
+def recebedados(cliente):
+#LOOP PARA RECEBER BANCO DE DADOS 
+    while True: 
+        try:
+            mensagem = conn.recv(1024).decode() #ACEITA 1024 BYTES
+            mensagemnome = nome + ': ' + mensagem
+            print(mensagemnome)
+        except:
+            print('Ocorreu algum erro ao receber as mensagens... Fechando conexao')
+            conm.close()
+            break
+    
+    
 
 
 #criação do socket e das informações do servidor
@@ -13,7 +29,7 @@ sock.bind((HOST, PORT))
 #127.0.0.1
 #O SERVIDOR ESTÁ ESCUTANDO...
 sock.listen()
-print(f"O servidor {HOST}{PORT} está ativo e aguardando conexões...")
+print(f"O servidor {HOST}:{PORT} está ativo e aguardando conexões...")
 
 #FUNÇÃO QUE É "ACESSA" PELO CONNECT DO CLIENTE, RETORNA 2 PARAMETROS 
 #CON É O SOCKET DO CLIENTE E O ENDER É O ENDEREÇO 
@@ -24,13 +40,21 @@ print(f"O servidor {HOST}{PORT} está ativo e aguardando conexões...")
 conn, ender = sock.accept()
 print("Conexão em ", ender)
 
-#LOOP PARA RECEBER BANCO DE DADOS 
-while True: 
-    dados = conn.recv(1024) #ACEITA 1024 BYTES
-    if not dados:
-        print("Fechando conexão...")
-        conn.close()
-        break
-    conn.sendall(dados)
-    
-    
+
+while True:
+    try:
+        conm, ender = sock.accept()
+        #ENDER[0] = IP ENDER[1] = PORTA
+        nome = conm.recv(50).decode() #RECEBER DADOS EM UTF-8
+        print(f'Conexâo com sucesso com {nome} - {ender[0]}:{ender[1]}')
+    except:
+        print('Algum erro de conexâo ocorreu... tentando novamente')
+        continue
+    threadCliente = threading.Thread(target=recebedados , args=[conm])
+    threadCliente.start()
+
+
+
+
+        
+        
