78402d599fc0182c0098aef5e19467c285306862
Adriano Vale <acsvalefilho@gmail.com>
2023-11-09 12:24:59 -0300

    .fodasse

--- .PUC/2º Semestre/Conectividade em Sistemas Ciberfísicos/cliente.py
+++ .PUC/2º Semestre/Conectividade em Sistemas Ciberfísicos/cliente.py
@@ -1,16 +1,58 @@
 import socket
+import threading
+import os
 
-#PRECISAMOS NOS CONECTAR NO SERVIDOR 
-HOST = '127.0.0.1' #LOCAL HOST
-PORT = 9991
 
-sock_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def limpaTerminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-#CLIENTE SOLOCITA A CONEXAO COM O SERVIDOR 
-sock_cliente.connect((HOST, PORT))
-#CLIENTE ENVIA DADOS PARA O SERVIDOR 
-sock_cliente.sendall(str.encode("Ola eu sou o cliente 1 !!!"))
+#função de recebimento de dados do cliente
+def recebeDados(conn):
+    try:
+        while True:
+            mensagem = conn.recv(1024).decode()
+            print(mensagem)
+    except:
+        print("Erro no recebimento de mensagens do servidor...")
+        return
 
-dados = socket_cliente.recv(1024)
-print('Menssagem recebida para teste: ')
-print(dados.decode())
+#INFORMAÇÕES DO SERVIDOR
+HOST = "127.0.0.1"
+PORT = 9999
+#QUERO ME CONECTAR EM 127.0.0.1:9999
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#COMO A CONEXÃO É TCP PRECISAMOS DE UM CONNECT
+sock.connect((HOST, PORT)) #CONNECT ACENDE O ACCEPT DO SERVIDOR
+#AGORA O CLIENTE ENVIA DADOS
+limpaTerminal()
+nome = input("Informe o seu nome: ")
+print('-----COMANDOS PRINCIPAIS-----')
+print('- /kill = sair do chat')
+print('-')
+print('-')
+print('-')
+print("----- CHAT INICIADO -----")
+#enviando nome para o servidor
+sock.sendall(str.encode(nome))
+
+#vamos inicializar a thread para o cliente ficar recebendo dados
+threadRecebeDados = threading.Thread(target=recebeDados, args=[sock])
+threadRecebeDados.start()
+
+#vamos abrir um loop para o cliente ficar enviando dados para o Servidor
+try:
+    while True:
+        mensagem = input('')
+        sock.sendall(str.encode(mensagem))
+        if mensagem == ('/kill'):
+            break
+        
+finally:
+    sock.close()
+    print("A conexão com o servidor foi finalizada")
+
+
+
+
+
--- .PUC/2º Semestre/Conectividade em Sistemas Ciberfísicos/server.py
+++ .PUC/2º Semestre/Conectividade em Sistemas Ciberfísicos/server.py
@@ -1,60 +1,68 @@
 import socket
 import threading
+import os
 
+def limpaTerminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def recebedados(cliente):
-#LOOP PARA RECEBER BANCO DE DADOS 
-    while True: 
+def recebeDados(conn,ender):
+    #recebimento do nome do cliente que teve a conexão aceita
+    try:
+        nome = conn.recv(50).decode() #50 é o tamanho do buffer
+        print(f"Conectado com {nome}, IP: {ender[0]}, PORTA: {ender[1]}")
+    except:
+        print("Ocorreu um erro durante o recebimento do nome. A conexão será encerrada")
+        return
+    while True:
         try:
-            mensagem = conn.recv(1024).decode() #ACEITA 1024 BYTES
-            mensagemnome = nome + ': ' + mensagem
-            print(mensagemnome)
+            mensagem = conn.recv(1024).decode()
+            mensagemNome = nome + " >> " + mensagem
+            print(mensagemNome)
+            #estamos fazendo um broadcast via aplicação (#todo: criar função broadcast)
+            try:
+                for cliente in lista_cliente:
+                    cliente.sendall(mensagemNome.encode())
+            except:
+                print("Ocorreu algum erro no envio dos dados...")
+                return
         except:
-            print('Ocorreu algum erro ao receber as mensagens... Fechando conexao')
-            conm.close()
+            print("Ocorreu algum erro na recepção dos dados, encerrando conexão.")
             break
-    
-    
 
-
-#criação do socket e das informações do servidor
-HOST = "127.0.0.1"
-PORT = 9991
-
-#IPV4 É O PARAMETRO AF_INET
-#TCP É O PARAMETRO SOCK_STREM
+HOST = 'localhost' #127.0.0.1
+PORT = 9999
+limpaTerminal()
+lista_cliente = []
+#inicializar o socket com seus parâmetros básicos (IPv4 e TCP)
+#AF_INET É IPV4
+#SOCK_STREAM É TCP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#PRECISAMOS VINCULAR O SOCKET SERVIDOR: ENDEREÇO IP : PORTA -> BIND
+#LOCALHOST:9999 COMO SERVIDOR
+sock.bind((HOST,PORT))
 
-sock.bind((HOST, PORT))
-#127.0.0.1
-#O SERVIDOR ESTÁ ESCUTANDO...
-sock.listen()
-print(f"O servidor {HOST}:{PORT} está ativo e aguardando conexões...")
-
-#FUNÇÃO QUE É "ACESSA" PELO CONNECT DO CLIENTE, RETORNA 2 PARAMETROS 
-#CON É O SOCKET DO CLIENTE E O ENDER É O ENDEREÇO 
-#ESSE É O PONTO QUE CONECTAMOS O CLIENTE 
-#PARA TERMOS VÁRIOS, PRECISAMOS DE UM LOOP INFINITO
-
-
-conn, ender = sock.accept()
-print("Conexão em ", ender)
-
+#AGORA O SERVIDOR COMEÇA A ESCUTAR CONEXÕES
+sock.listen() #abre a porta
+print(f"O Servidor {HOST}:{PORT} está aguardando conexões")
 
 while True:
+    #ACEITAMOS A CONEXÃO de vários clientes
+    #conn é o socket do cliente
+    #ender é a porta do cliente
     try:
-        conm, ender = sock.accept()
-        #ENDER[0] = IP ENDER[1] = PORTA
-        nome = conm.recv(50).decode() #RECEBER DADOS EM UTF-8
-        print(f'Conexâo com sucesso com {nome} - {ender[0]}:{ender[1]}')
+        conn, ender = sock.accept()
+        #adicionado cliente a lista de clientes
+        lista_cliente.append(conn)
     except:
-        print('Algum erro de conexâo ocorreu... tentando novamente')
+        print('Ocorreu um erro durante o ACCEPT() na conexão com um novo usuário')
         continue
-    threadCliente = threading.Thread(target=recebedados , args=[conm])
+    
+    
+    #LOOP INFINITO PARA RECEBIMENTO DE DADOS: VAI PARA A FUNÇÃO QUE SERÁ EXECUTADA PELA THREAD
+    threadCliente = threading.Thread(target=recebeDados, args=[conn,ender])
     threadCliente.start()
+    
+  
+     
+   
 
-
-
-
-        
-        
